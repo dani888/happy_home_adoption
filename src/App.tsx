fb635f136598d1eec933416641c059a0e526bb10
dani888 <daniel.werminghausen@gmail.com>
2026-07-12 11:57:46 -0400

    wip:3 fix mobile issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
+import ScrollToTop from "./components/ScrollToTop";
 import HomePage from "./pages/HomePage";
 import DogsPage from "./pages/DogsPage";
 import CatsPage from "./pages/CatsPage";
@@ -9,6 +10,7 @@ import AboutPage from "./pages/AboutPage";
 function App() {
   return (
     <Layout>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/dogs" element={<DogsPage />} />
